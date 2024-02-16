5df97979e1f486c48459df995afead0180a5dd68
Fawaz Al Ahmad <fawaz.amd11@gmail.com>
2024-02-16 18:30:33 +0200

    added routes configuration

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/components/home/home.component';
+import { CategoryNavbarComponent } from './layout/components/category-navbar/category-navbar.component';
+import { SinglePostComponent } from './pages/components/single-post/single-post.component';
+import { SingleCategoryComponent } from './pages/components/single-category/single-category.component';
+import { AboutUsComponent } from './pages/components/about-us/about-us.component';
+import { ContactUsComponent } from './pages/components/contact-us/contact-us.component';
+import { TermsAndConditionsComponent } from './pages/components/terms-and-conditions/terms-and-conditions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'category', component: SingleCategoryComponent },
+  { path: 'post', component: SinglePostComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'terms-conditions', component: TermsAndConditionsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
