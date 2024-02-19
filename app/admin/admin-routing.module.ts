d119306ef13fd6e73c1874c52ec54eede7d82ef0
Fawaz Al Ahmad <fawaz.amd11@gmail.com>
2024-02-19 18:54:19 +0200

    created and added markup to the admin dashboard

--- app/admin/admin-routing.module.ts
+++ app/admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainComponent } from "./components/main/main.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 const routes: Routes = [
   {path:'', component: MainComponent},
+  {path:'dashboard', component: DashboardComponent},
 ]
 
 @NgModule({
