f3f324cac7e9b5ccb06e40351343620b4d18cffe
Fawaz Al Ahmad <fawaz.amd11@gmail.com>
2024-02-20 11:29:57 +0200

    installed angularfire and set up firebase in app module

--- environments/environment.development.ts
+++ environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production : false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAViBezv6JZjWbePwNIMGVh4CMIvCJEtFM",
+    authDomain: "ng-blog-a0627.firebaseapp.com",
+    projectId: "ng-blog-a0627",
+    storageBucket: "ng-blog-a0627.appspot.com",
+    messagingSenderId: "588367182089",
+    appId: "1:588367182089:web:4ce510a42a04841bd5bbd8"
+  }
+};
