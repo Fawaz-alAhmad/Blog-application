82a64a44727834ab484d253c1eff8d8772289285
Fawaz Al Ahmad <fawaz.amd11@gmail.com>
2024-02-20 15:04:44 +0200

    created categories component,added a form and connected it to Firestore

--- app/admin/admin-routing.module.ts
+++ app/admin/admin-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainComponent } from "./components/main/main.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { CategoriesComponent } from "./components/categories/categories.component";
 
 const routes: Routes = [
-  {path:'', component: MainComponent},
-  {path:'dashboard', component: DashboardComponent},
+  {path:'', component: DashboardComponent},
+  {path:'categories', component: CategoriesComponent},
 ]
 
 @NgModule({
